287e44db91383023f31fcfbb22ee8823ef592184
VEDANT BHARAD <92723979+Vedantbharad2603@users.noreply.github.com>
2024-04-04 09:15:12 +0530

    db change

--- config/db.config.js
+++ config/db.config.js
@@ -1,9 +1,10 @@
 const MYSQL_DB_CONFIG = {
-    HOST: "localhost",
-    USER: "root",
+    HOST: "sql6.freesqldatabase.com",
+    USER: "sql6696576",
     PORT: 3306,
-    PASSWORD: "",
-    DB: "main_loreto_db",
+    PASSWORD: "8iAkg4e2pp",
+    DB:"sql6696576",
+    // DB: "main_loreto_db",
 };
 module.exports = {
     MYSQL_DB_CONFIG,
