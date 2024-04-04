64b010da9b9a8d0c59db4a1bea8b94a61aac1382
VEDANT BHARAD <92723979+Vedantbharad2603@users.noreply.github.com>
2024-04-04 09:54:16 +0530

    9:54

--- config/db.config.js
+++ config/db.config.js
@@ -1,14 +1,9 @@
 const MYSQL_DB_CONFIG = {
-    HOST: "sql6.freesqldatabase.com",
-    USER: "sql6696576",
+    HOST: "localhost",
+    USER: "root",
     PORT: 3306,
-    PASSWORD: "8iAkg4e2pp",
-    DB:"sql6696576",
-    // HOST: "localhost",
-    // USER: "root",
-    // PORT: 3306,
-    // PASSWORD: "",
-    // DB: "main_loreto_db",
+    PASSWORD: "",
+    DB: "main_loreto_db",
 };
 module.exports = {
     MYSQL_DB_CONFIG,
